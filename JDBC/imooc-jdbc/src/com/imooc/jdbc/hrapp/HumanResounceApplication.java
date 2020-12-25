cf58099ede7e5e00861d7be029a68230cdce6ff2
降蓝 <2642475449@qq.com>
2020-12-25 22:29:03 +0800

    更新说明

---
+++ JDBC/imooc-jdbc/src/com/imooc/jdbc/hrapp/HumanResounceApplication.java
@@ -0,0 +1,22 @@
+package com.imooc.jdbc.hrapp;
+
+import com.imooc.jdbc.hrapp.command.Command;
+import com.imooc.jdbc.hrapp.command.QueryCommend;
+
+import java.util.Scanner;
+
+public class HumanResounceApplication {
+    public static void main(String[] args) {
+        System.out.println("1-查询部门员工");
+        Scanner in = new Scanner(System.in);
+
+        Integer anInt = in.nextInt();
+
+        switch (anInt) {
+            case 1:
+                Command command = new QueryCommend();
+                command.execute();
+        }
+
+    }
+}
--- JDBC/imooc-jdbc/src/com/imooc/jdbc/hrapp/command/QueryCommend.java
+++ JDBC/imooc-jdbc/src/com/imooc/jdbc/hrapp/command/QueryCommend.java
@@ -4,50 +4,51 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class QueryCommend implements Command {
+
+
     @Override
     public void execute() {
         System.out.println("请输入部门");
-        Scanner in = new Scanner(System.in);
-        String pdname = in.next();
-        Connection conn = null;
-        Statement stmt = null;
+        //键盘输入
+        Scanner scannerIn = new Scanner(System.in);
+        //键盘输入后得到的字符串
+        String pdname = scannerIn.next();
+        Connection connection = null;
+        Statement statement = null;
         ResultSet rs = null;
-        try {
-            //第一步：把想要连接的数据库驱动加载入JVM，如加载mysql数据库驱动类可以通过`Class.forName("com.mysql.cj.jdbc.Driver");`加载并注册JDBC驱动
+
+        try {//第一步：把想要连接的数据库驱动加载入JVM，如加载mysql数据库驱动类可以通过`Class.forName("com.mysql.cj.jdbc.Driver");`加载并注册JDBC驱动
             Class.forName("com.mysql.cj.jdbc.Driver");
             //第二步：使用`DriverManager.getConnection(String url , String username , String password)`创建数据库连接
-            conn = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/imooc?useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai",
                     "root",
                     "448866293"
             );
-
             //第三步：利用**上一步的数据库连接**创建Statement
-             stmt = conn.createStatement();
+            statement = connection.createStatement();
             //结果集
-             rs = stmt.executeQuery("SELECT * from employee WHERE dname='" + pdname + "'");
+            rs = statement.executeQuery("SELECT * FROM employee WHERE dname = '" + pdname + "'");
             //第四步：遍历查询结果
             while (rs.next()) {
-                Integer eno = rs.getInt(1);
+                Integer eno = rs.getInt(1);//表示把数据表的第一行提取出来
                 String ename = rs.getString("ename");
                 float salary = rs.getFloat("salary");
                 String dname = rs.getString("dname");
                 System.out.println(dname + "-" + eno + "-" + ename + "-" + salary);
             }
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
 
             //第五步：关闭连接，释放资源
             try {
-                conn.close();
+                connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
