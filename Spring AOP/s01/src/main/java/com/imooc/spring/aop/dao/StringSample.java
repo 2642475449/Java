0f71804889d44164bf8278ddd5bdacef2a008212
降蓝 <2642475449@qq.com>
2021-02-04 10:00:02 +0800

    AOP通知

---
+++ Spring AOP/s01/src/main/java/com/imooc/spring/aop/dao/StringSample.java
@@ -0,0 +1,9 @@
+package com.imooc.spring.aop.dao;
+
+public class StringSample {
+    public static void main(String[] args) {
+        String a = "chenssy";
+        String b = "chenssy";
+        System.out.println(a == b);
+    }
+}
